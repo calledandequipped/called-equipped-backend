d4d6d500760a2e710b62658fd14c3499339cb7c2
calledandequipped <calledandequippedcourse@gmail.com>
2025-08-04 08:49:32 -0700

    Add files via upload
    update to success.html

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.post('/api/create-checkout-session', async (req, res) => {
         quantity: 1,
       }],
       mode: 'payment',
-      success_url: `${process.env.FRONTEND_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+      success_url: `${process.env.FRONTEND_URL}/success.html?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${process.env.FRONTEND_URL}/cancel`,
       customer_email: email,
       metadata: {
